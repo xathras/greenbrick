08912d0ddca1a35e809f36f81a4b2d1b2d1637d9
Andrew <akothmann@iexposure.com>
2014-01-24 10:31:32 -0600

    add connection error handling for the redis store

--- lib/greenbrick/storage/redis.rb
+++ lib/greenbrick/storage/redis.rb
@@ -6,27 +6,35 @@ module Greenbrick
       def initialize( key_space , redis_client_options = {} )
         @key_space = key_space
         @client = ::Redis.new redis_client_options
+        @cache
       end
 
       def []=(key,value)
         @client.hset @key_space , key , value
+        recache
       end
 
       def [](key)
         @client.hget @key_space , key
+      rescue BaseConnectionError
+        @cache[key]
       end
 
       def each
-        hash.each
+        recache
+        @cache.each
       end
 
       def length
-        hash.length
+        recache
+        @cache.length
       end
 
     private
-      def hash
-        @client.hgetall @key_space
+      def recache
+        @cache = @client.hgetall @key_space
+      rescue BaseConnectionError
+        @cache
       end
     end
   end
